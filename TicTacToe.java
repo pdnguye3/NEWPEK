e5ac40e0b66e1bcac6672bc7a663fc734bea211d
Phillip Nguyen <pdnguye3@asu.edu>
2018-10-20 07:57:02 -0700

    Create TicTacToe.java
    premade skeleton

---
+++ TicTacToe.java
@@ -0,0 +1,41 @@
+/**
+*  TicTacToe. A playable text version of TicTacToe made as a test
+*  for collaboration in GitHub.
+*
+* @author Nguyen P, Egnew E, Wood K.
+* @version 20.October.2018
+*/
+
+public class TicTacToe {
+
+  int[][] board = new int[3][3];
+
+  /**
+  * main. entry point for text TicTacToe
+  * @param args
+  */
+  public void main(String[] args) {
+ 
+    
+  
+  }
+  
+  public boolean checkMove(int move, int x, int y) {
+  
+  
+  
+  }
+
+  public void updateBoard(int move, int x, int y) {
+  
+    board[y][x] = move;
+  
+  }
+
+  public boolean checkWin() {
+  
+  
+  
+  }
+
+}
